6525453cf9e8205939ab478b81046d089db006b1
M4hfd <83091151+M4hfd@users.noreply.github.com>
2022-12-08 16:53:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 #Шифровки
 message = input("Введите сообщение: ")
 #1
-cipher_one = message
 cipher_one = message.replace('и', 'o')
-print("Первый шифр: " + str(cipher_one))
+print("Первый шифр: " + cipher_one)
 #2
-cipher_two = message
-print("Второй шифр: " + str(cipher_two[::-1]))
+cipher_two = message[::-1]
+print("Второй шифр: " + cipher_two)
 #3
 cipher_three = message[::2] + message[1::2]
-print("Третий шифр: "+ str(cipher_three))
+print("Третий шифр: "+ cipher_three)
 #4
-cipher_four = message
-first_letter = cipher_four[0]
-last_letter = cipher_four[-1]
+first_symbol = message[0]
+last_symbol = message[-1]
 
-length = len(cipher_four)
+length = len(message)
 
-cipher_four = last_letter + message[1:length-1] + first_letter
-print("Четвертый шифр: " + str(cipher_four))
+cipher_four = last_symbol + message[1:length-1] + first_symbol
+print("Четвертый шифр: " + cipher_four)
 #5
 cipher_five = message
 half_length = len(message)//2
-
 cipher_five = message[half_length:] + message[:half_length]
-print("Пятый шифр: " + str(cipher_five))
+print("Пятый шифр: " + cipher_five)
 #Рассшифровки
 #1 
 cipher_one = message.replace('о', 'и')
-print("Первая расшифровка: " + str(cipher_one))
+print("Первая расшифровка: " + cipher_one)
 #2
-print("Вторая расшифровка: " + str(cipher_two[::1]))
+print("Вторая расшифровка: " + cipher_two[::-1])
 #3
 result = '' 
 even = message[::2]
@@ -45,16 +42,17 @@ for symbol in even:
 print('Третья расшифровка:',result)
 #4
 first_letter = cipher_four[-1]
-last_letter = cipher_four[-0]
+last_letter = cipher_four[0]
 
 length = len(cipher_four)
 
-cipher_four = first_letter + message[1:length-1] + last_letter
-print("Четвертая расшифровка: " + str(cipher_four))
+cipher_four = first_symbol + message[1:length-1] + last_symbol
+print("Четвертая расшифровка: " + cipher_four)
 #5
 half_length = len(message)//2
-cipher_five = message[:half_length] + message[half_length:]
-print("Пятая расшифровка: " + str(cipher_five))
+cipher_five = cipher_five[half_length:] + cipher_five[:half_length]
+print("Пятая расшифровка: " + cipher_five)
+
 
 
 
